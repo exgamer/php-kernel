24939219cffc7a9c151217e15467c15c168450a3
Olzhas Kulzhambekov <exgamer@live.ru>
2021-01-27 13:20:30 +0600

    init

---
+++ src/Services/ReadService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Citizenzet\Php\Core\Services;
+
+use Citizenzet\Php\Core\Interfaces\DataReadInterface;
+
+/**
+ * Class ReadService
+ * @package Citizenzet\Php\Core\Services
+ * @author citizenzet <exgamer@live.ru>
+ */
+abstract class ReadService extends Service implements DataReadInterface
+{
+
+}
